b28e74d878e8c014673dc46bc4ac79961aa03be5
Kittipod Lambangchang <tonkittipod@gmail.com>
2025-12-26 18:09:45 +0700

    feat: init project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by กิตติพศ หลำบางช้าง on 26/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
